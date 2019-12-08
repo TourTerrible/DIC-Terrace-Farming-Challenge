6cb9224b86f8b754e98f30f4801ddb2d2b5796f3
yoimABD <ahad.jnv22@gmail.com>
2019-12-08 17:19:33 +0530

    control for each task updated

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -4,7 +4,7 @@ void setup() {
   // Begin the Serial at 9600 Baud
   Serial.begin(9600);
 }
-
+//{start,pump,side,center,seeding,plough,harvest}
 void loop() {
   Serial.readBytes(data,7); 
   Serial.print(data[0]);
